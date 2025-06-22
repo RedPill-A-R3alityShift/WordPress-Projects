09aba25c030bb9fdf683293e523da8232952ce04
RedPill-A-R3alityShift <wi.it.professionals@gmail.com>
2025-06-22 15:11:40 -0500

    Initial commit

---
+++ Blank Theme Template/Blank Theme/footer.php
@@ -0,0 +1,16 @@
+<!--		<div id="footer">
+
+			<h3>This is the FOOTER PHP<br>
+			&copy;<?php echo date("Y"); echo " "; bloginfo('name'); ?>
+			</h3>
+		</div>
+
+	</div>
+
+	<?php wp_footer(); ?>
+	-->
+	<!-- Don't forget analytics -->
+	
+</body>
+
+</html>
---
+++ Blank Theme Template/Blank Theme/home.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ 	Template Name: Home Page Template
+*/
+?>
+
+<?php get_header(); ?>
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		
+
+			
+
+			
+
+			<div>
+				<?php the_content(); ?>
+			</div>
+
+			
+
+		
+
+	<?php endwhile; ?>
+
+	
+
+	<?php else : ?>
+
+		<h2>Not Found</h2>
+
+	<?php endif; ?>
+	
